34da9bff37f760eef224cfcb6576da9cffb3b3d9
Sam Baltz <samuelabaltz@gmail.com>
2024-03-29 12:13:39 -0800

    Iterate through filenames for dynamic routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
+const {readdir} = require("fs");
+const {join} = require("path");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'US National Intelligence Estimates' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'US National Intelligence Estimates'});
 });
+
+const directoryPath = join(__dirname, 'files/');
+readdir(directoryPath, function (err, files) {
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    }
+    files.forEach(function (file) {
+        // console.log(file.replace(/\.pdf$/, ""))
+        router.get(`/${file.replace(/\.pdf$/, "")}`, function (req, res, next) {
+            res.render('index', {title: 'US National Intelligence Estimates'});
+        });
+    })
+})
 
 module.exports = router;
