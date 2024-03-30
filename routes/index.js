31f4e04768ef589fba3982252c94730f055de1f7
Sam Baltz <samuelabaltz@gmail.com>
2024-03-30 00:55:26 -0800

    Get pdf file showing

--- routes/index.js
+++ routes/index.js
@@ -8,17 +8,25 @@ router.get('/', function (req, res, next) {
     res.render('index', {title: 'US National Intelligence Estimates'});
 });
 
-const directoryPath = join(__dirname, 'files/');
-readdir(directoryPath, function (err, files) {
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    }
-    files.forEach(function (file) {
-        // console.log(file.replace(/\.pdf$/, ""))
-        router.get(`/${file.replace(/\.pdf$/, "")}`, function (req, res, next) {
-            res.render('index', {title: 'US National Intelligence Estimates'});
-        });
-    })
-})
+// const directoryPath = join(__dirname, 'files/');
+// readdir(directoryPath, function (err, files) {
+// if (err) {
+//     return console.log('Unable to scan directory: ' + err);
+// }
+// files.forEach(function (file) {
+//     router.get(`/${file.replace(/\.pdf$/, "")}`, function (req, res, next) {
+//         res.render('pdf', {pdfpath: file, pdfname: file});
+//     });
+// })
+// router.get('/test', function (req, res, next) {
+//     res.render('pdf', {pdfname: 'test'})
+// })
+
+// })
+
+
+router.get('/test', function (req, res, next) {
+    res.render('pdf', {title: 'US National Intelligence Estimates'});
+});
 
 module.exports = router;
